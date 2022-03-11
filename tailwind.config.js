52cc5e3fc08ec7c5e765ae8844d6a0c51093df2b
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-11 18:07:35 -0500

    FEAT: change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {
       boxShadow: {
         '3xl': '0 15px 60px -10px rgb(11, 11, 11, 0.16);',
