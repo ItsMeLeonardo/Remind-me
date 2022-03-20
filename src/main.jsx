7f0331d7404a8981b05c718bd06b3dfcf66a9842
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-19 20:41:36 -0500

    FEAT: add recoil

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RecoilRoot } from 'recoil'
+
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>,
+  document.getElementById('root'),
 )
