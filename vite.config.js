d2f3638320553690ca0df45438b58b9e17cdd884
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-23 08:18:27 -0500

    FEAT: add alias to paths

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
